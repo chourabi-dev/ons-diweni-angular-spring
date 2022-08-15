a42a1be4afa9111bcd486f15f0381b693c99be6a
chourabi-dev <chourabitahers62@gmail.com>
2022-08-15 15:20:53 +0100

    latest day 2

--- tuto/src/app/app.component.ts
+++ tuto/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Procuct } from './models/Products';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,46 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   name:string = "chourabi taher";
+
+  year = 2022;
+
+  email;
+
+
+  today = new Date();
+
+
+  age:number = 90;
+
+
+
   
+
+  employee = { name:"chourabi taher", age:80, email:"tchourabi@gmail.com" };
+
+
+  /******************************************************************************************* */
+
+
+
+  employees:string[] = [ 'chourabi taher','chourabi taher 1','chourabi taher 2' ];
+
+
+
+  products:Procuct[] = [
+    new Procuct(5,"samsung s21",2500,10), // { id:5, title:"HP headset", price:20, stock:20 }
+    new Procuct(6,"HP pavillon",2000,0),
+    new Procuct(7,"LG smart TV",1800,0)
+
+    
+  ];
+
+
+  /*products2:Procuct[] = [
+    { id:5, title:"HP headset", price:20, stock:20 },
+    { id:5, title:"HP headset", price:20, stock:20 },
+    
+    
+  ];*/
 
 }
---
+++ tuto/src/app/models/Products.ts
@@ -0,0 +1,13 @@
+export class Procuct{
+    id:number;
+    title:string;
+    price:number;
+    stock:number;
+
+    constructor(id,title,price,stock){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+    }
+}
